505f26a291fe19a5b034a8c6b48cc04b24b125fd
Daan Zonneveld <d.zonneveld@wearetriple.com>
2025-02-26 14:21:44 +0100

    chore: upload source maps in build process

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,14 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import path from 'node:path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import type { PluginOption } from 'vite';
+
+const sentryConfig: PluginOption[] = process.env.SENTRY_AUTH_TOKEN
+  ? [
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: 'triple',
+        project: 'local-llm-desktop',
+        url: 'https://sentry.wearetriple.com/',
+      }),
+    ]
+  : [];
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), ...sentryConfig],
     resolve: { alias: { '@shared': path.resolve('src/shared') } },
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), ...sentryConfig],
     resolve: { alias: { '@shared': path.resolve('src/shared') } },
   },
   renderer: {
@@ -18,6 +32,6 @@ export default defineConfig({
         '@shared': path.resolve('src/shared'),
       },
     },
-    plugins: [react()],
+    plugins: [react(), ...sentryConfig],
   },
 });
